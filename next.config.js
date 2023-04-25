4cf9914f9c2f8f965ca3396412e3d99ddd6a2d08
Drew Bredvick <dbredvick@gmail.com>
2023-04-24 20:27:54 -0500

    working example

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  rewrites() {
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: '/blog/:path*',
+          destination: `https://blog.usegravity.app/:path*`,
+        },
+        {
+          source: '/:path*',
+          destination: `https://blog.usegravity.app/:path*`,
+        },
+      ],
+    }
+  }
 }
 
 module.exports = nextConfig
